ec3df38601406003eb64e1f4ee3e88a39cbd495a
Yingchun Guo <yingchun.guo@intel.com>
2024-05-22 20:05:53 -0700

    enable CI integration test for charts
    Signed-off-by: Yingchun Guo <yingchun.guo@intel.com>

---
+++ .github/workflows/scripts/chart-integration/chart-test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set -x
+
+# get the current user id
+# get the current user name
+export USER_ID=$(whoami)
+export PATH=/home/$USER_ID/.local/bin:$PATH # add huggingface-cli to PATH
+
+CHART_NAME=$1
+ROLLOUT_TIMEOUT_SECONDS=600s
+KUBECTL_TIMEOUT_SECONDS=60s
+RELEASE_NAME=${CHART_NAME}$(date +%Y%m%d%H%M%S)
+NAMESPACE=${CHART_NAME}-$(date +%Y%m%d%H%M%S)
+
+SCRIPT_DIR=$(dirname -- $0)
+source $SCRIPT_DIR/lib.sh
+
+# step 1 Run init
+echo "Testing chart $CHART_NAME, init environment..."
+# call init_chart function
+init_${CHART_NAME}
+echo "Testing chart $CHART_NAME, init environment done!"
+
+# step 2 Helm install
+echo "Testing chart $CHART_NAME, installing chart with helm..."
+if helm install --create-namespace --namespace $NAMESPACE --wait --timeout "$ROLLOUT_TIMEOUT_SECONDS" $RELEASE_NAME .; then
+    echo "Testing chart $CHART_NAME, installing chart with helm done!"
+else
+    echo "Failed to install chart $CHART_NAME!"
+    exit 1
+fi
+
+# step 3 Wait for pod ready
+# echo "Testing chart $CHART_NAME, waiting for pod ready..."
+# if kubectl rollout status deployment --namespace "$NAMESPACE" --timeout "$ROLLOUT_TIMEOUT_SECONDS"; then
+#     echo "Testing chart $CHART_NAME, waiting for pod ready done!"
+#     return_code=0
+# else
+#     echo "Timeout waiting for pods in namespace $NAMESPACE to be ready!"
+#     return_code=1
+# fi
+
+# step 4 Validate
+# if return_code is 0, then validate, call validate_${CHART_NAME}
+if [ $return_code -eq 0 ]; then
+    echo "Testing chart $CHART_NAME, validating..."
+    # check the output of validate_${CHART_NAME} to determine if the validation is successful
+    # if the output of validate_${CHART_NAME} contains "Response check succeed!", then the validation is successful
+    output=$(validate_${CHART_NAME})
+    if [[ $output == *"succeed!"* ]]; then
+        echo "Testing chart $CHART_NAME, validating done!"
+    else
+        echo "Testing chart $CHART_NAME, validating failed!"
+        return_code=1
+    fi
+fi
+
+# step 5 Clean up
+echo "Testing chart $CHART_NAME, cleaning up..."
+helm uninstall $RELEASE_NAME --namespace $NAMESPACE
+if ! kubectl delete ns $NAMESPACE --timeout=$KUBECTL_TIMEOUT_SECONDS; then
+    # if kubectl delete timeout, force delete
+    kubectl delete pods --namespace $NAMESPACE --force --grace-period=0 --all
+    kubectl delete ns $NAMESPACE --force --grace-period=0 --timeout=$KUBECTL_TIMEOUT_SECONDS
+fi
+echo "Testing chart $CHART_NAME, cleaning up done!"
+
+exit $return_code
