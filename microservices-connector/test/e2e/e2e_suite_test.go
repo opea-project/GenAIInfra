5d323ea242f3c664ddb68e1b7e5bb7ba871a7140
zhlsunshine <huailong.zhang@intel.com>
2024-05-23 15:37:47 +0800

    Add genai-microservice-connectors(GMC) into GenAIInfra.
    Signed-off-by: zhlsunshine <huailong.zhang@intel.com>
    Co-authored-by: irisdingbj <shaojun.ding@intel.com>
    Co-authored-by: KfreeZ <kefei.zhang@intel.com>

---
+++ microservices-connector/test/e2e/e2e_suite_test.go
@@ -0,0 +1,21 @@
+/*
+* Copyright (C) 2024 Intel Corporation
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+package e2e
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+// Run e2e tests using the Ginkgo runner.
+func TestE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	fmt.Fprintf(GinkgoWriter, "Starting gmconnector suite\n")
+	RunSpecs(t, "e2e suite")
+}
