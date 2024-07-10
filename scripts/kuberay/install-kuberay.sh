8723b9ff37cb430c06d155c882bee399c4488069
Wu, Xiaochang <xiaochang.wu@intel.com>
2024-07-10 07:56:31 +0000

    update
    Signed-off-by: Wu, Xiaochang <xiaochang.wu@intel.com>

---
+++ scripts/kuberay/install-kuberay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+helm repo add kuberay https://ray-project.github.io/kuberay-helm/
+helm repo update
+
+# Install both CRDs and KubeRay operator v1.1.1.
+helm install kuberay-operator kuberay/kuberay-operator --version 1.1.1
+
+# Confirm that the operator is running in the namespace `default`.
+kubectl get pods
+# NAME                                READY   STATUS    RESTARTS   AGE
+# kuberay-operator-7fbdbf8c89-pt8bk   1/1     Running   0          27s
