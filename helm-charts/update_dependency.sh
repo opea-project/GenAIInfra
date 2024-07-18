fbdb1da9bb40b810eb6615685883445c1c952f29
Lianhao Lu <lianhao.lu@intel.com>
2024-07-18 12:12:38 +0800

    helm: support remove helm dependency
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>

--- helm-charts/update_dependency.sh
+++ helm-charts/update_dependency.sh
@@ -7,5 +7,7 @@ UPD_DIR=$(cd $(dirname "$0") && pwd)
 for chart in ${UPD_DIR}/common/*
 do
 	echo "Update dependency for `basename $chart`..."
+        rm -f ${chart}/Chart.lock
+        rm -rf ${chart}/charts/
 	helm dependency update ${chart}
 done
