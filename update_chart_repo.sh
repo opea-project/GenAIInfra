e4adb0848f47a465d2df79e7db064a8e2c874855
Dolpher Du <dolpher.du@intel.com>
2024-08-26 05:35:59 +0000

    v0.9 charts release
    Signed-off-by: Dolpher Du <dolpher.du@intel.com>

--- update_chart_repo.sh
+++ update_chart_repo.sh
@@ -3,6 +3,15 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+# Steps for new helm charts release v0.9rc as example.
+# This should be implemented in CICD workflow.
+# 0. Charts tag updated for this branch
+# 1. git clone -b https://github.com/opea-project/GenAIInfra.git
+# 2. cd GenAIInfra; git switch v0.9rc; cp -rf helm-charts ../  # Copy the helm-charts directory
+# 3. git switch charts-release; mv ../helm-charts . # Switch to charts-release and copy back the helm-charts directory
+# 4. ./update_chart_repo.sh; rm -rf helm-charts
+# 5. git add .; git commit -s -m "v0.9 charts release"; git push
+
 UPD_DIR=$(cd $(dirname "$0") && pwd)
 CHART_DIR=./helm-charts
 
@@ -32,4 +41,5 @@ do
 done
 
 # Update Index
+mv index.yaml index.old
 helm repo index .
