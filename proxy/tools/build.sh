e81ac98dc5b7d7e49d66178da5d1f28f23b2b1a8
Xie Zhihao <zhihao.xie@intel.com>
2024-08-28 18:19:34 +0000

    proxy: support Docker build
    Signed-off-by: Xie Zhihao <zhihao.xie@intel.com>

---
+++ proxy/tools/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+BUILD_TAG=${BUILD_TAG:-75238004b0fcfd8a7f71d380d7a774dda5c39622}
+
+# Build build image.
+pushd tools
+docker build . --build-arg="BUILD_TAG=${BUILD_TAG}" -f Dockerfile-build -t opea/envoy-build-ubuntu:${BUILD_TAG}
+popd
+
+# Build proxy in the container.
+mkdir -p /tmp/proxy-docker-build
+docker run -it \
+           --rm \
+           -v /tmp/proxy-docker-build:/root/.cache \
+           -v $PWD:/source \
+           opea/envoy-build-ubuntu:${BUILD_TAG} \
+           "/bin/bash" "-c" "cd /source && export PATH=/opt/llvm/bin:\$PATH && bazel build --config docker -c opt envoy && mkdir -p bin && cp -f bazel-bin/envoy bin/envoy"
