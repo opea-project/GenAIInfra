bef88f3f749220d13a2529c1198d2112ba1f2f45
Dolpher Du <dolpher.du@intel.com>
2024-09-20 05:19:01 +0000

    Add release 1.0.0 helm charts
    Signed-off-by: Dolpher Du <dolpher.du@intel.com>

--- update_chart_repo.sh
+++ update_chart_repo.sh
@@ -11,6 +11,8 @@
 # 3. git switch charts-release; mv ../helm-charts . # Switch to charts-release and copy back the helm-charts directory
 # 4. ./update_chart_repo.sh; rm -rf helm-charts
 # 5. git add .; git commit -s -m "v0.9 charts release"; git push
+#  For 1.0 and later, include the gmc helm chart too
+# cd microservice-connector/; helm package helm; cp gmc-1.0.0.tgz
 
 UPD_DIR=$(cd $(dirname "$0") && pwd)
 CHART_DIR=./helm-charts
@@ -40,6 +42,12 @@ do
 	helm package $chart
 done
 
+# rm helm-charts
+rm -rf helm-charts
+
 # Update Index
-mv index.yaml index.old
+# mv index.yaml index.old
 helm repo index .
+
+# Insert copyright to avoid warnings
+sed -i '1s/^/# Copyright (C) 2024 Intel Corporation\n# SPDX-License-Identifier: Apache-2.0\n\n/' index.yaml
