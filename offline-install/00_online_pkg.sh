bd5ff18764b2ec0298c4b91a4b73aa1573888954
Guo Ruijing <ruijing.guo@intel.com>
2024-11-12 00:01:20 -0800

    fix typo for docker.io installation
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- offline-install/00_online_pkg.sh
+++ offline-install/00_online_pkg.sh
@@ -3,7 +3,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-sudo apt-get install -y dockder.io git
+sudo apt-get install -y docker.io git
 sudo snap install helm --classic
 wget https://arxiv.org/pdf/2212.04088 -o 2212.pdf
 rm -rf GenAIInfra
