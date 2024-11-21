777cc2f758e46ff57c7664355c47faae53f4229c
Guo Ruijing <ruijing.guo@intel.com>
2024-11-21 03:18:09 -0800

    support download from huggingface cli
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ offline-install/02.a_online_models.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+registry=registry:5000
+hf_token="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"
+http_proxy=""
+https_proxy=""
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -U "huggingface_hub[cli]"
+export HF_ENDPOINT=https://hf-mirror.com
+huggingface-cli login --token $hf_token
+huggingface-cli download BAAI/bge-base-en-v1.5 --local-dir data
+huggingface-cli download BAAI/bge-reranker-base --local-dir data
+huggingface-cli download Intel/neural-chat-7b-v3-3 --local-dir data
+
+#docker build . -t $registry/opea/models:latest
+#docker push $registry/opea/models:latest
