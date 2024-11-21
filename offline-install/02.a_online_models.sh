f2eb9ec0e43abfff93facbda5d2ed4332f02fed3
Guo Ruijing <ruijing.guo@intel.com>
2024-11-21 04:23:47 -0800

    update download dir
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- offline-install/02.a_online_models.sh
+++ offline-install/02.a_online_models.sh
@@ -13,9 +13,9 @@ source venv/bin/activate
 pip install -U "huggingface_hub[cli]"
 export HF_ENDPOINT=https://hf-mirror.com
 huggingface-cli login --token $hf_token
-huggingface-cli download BAAI/bge-base-en-v1.5 --local-dir data
-huggingface-cli download BAAI/bge-reranker-base --local-dir data
-huggingface-cli download Intel/neural-chat-7b-v3-3 --local-dir data
+huggingface-cli download BAAI/bge-base-en-v1.5 --local-dir data/models--BAAI--bge-base-en-v1.5
+huggingface-cli download BAAI/bge-reranker-base --local-dir data/models--BAAI--bge-base-en-v1.5
+huggingface-cli download Intel/neural-chat-7b-v3-3 --local-dir data/models--Intel--neural-chat-7b-v3-3
 
 #docker build . -t $registry/opea/models:latest
 #docker push $registry/opea/models:latest
