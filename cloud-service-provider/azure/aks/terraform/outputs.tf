8f8e62733391f59ce5cf7047fcf82e50f048d21e
kkrishTa <Kamalakara_T@infosys.com>
2024-12-06 17:57:32 +0530

    AKS Deployment for OPEA applications

---
+++ cloud-service-provider/azure/aks/terraform/outputs.tf
@@ -0,0 +1,21 @@
+output "cluster_endpoint" {
+  description = "Endpoint for AKS control plane"
+  #sensitive = false
+  sensitive = true
+  value       = azurerm_kubernetes_cluster.main.kube_config.0.host
+}
+
+output "oidc_issuer_url" {
+  description = "The URL for the OpenID Connect issuer"
+  value       = azurerm_kubernetes_cluster.main.oidc_issuer_url
+}
+
+output "location" {
+  description = "Azure region"
+  value       = var.location
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = azurerm_kubernetes_cluster.main.name
+}
