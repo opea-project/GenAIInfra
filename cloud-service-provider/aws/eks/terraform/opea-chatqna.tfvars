1e10b9d33060ef30c5724d35acc73c13853f5eff
Ezequiel Lanza <ezequiel.lanza@gmail.com>
2025-03-31 17:31:34 +0100

    Update AWS region configuration and clean up terraform files
    Signed-off-by: Ezequiel Lanza <ezequiel.lanza@gmail.com>

--- cloud-service-provider/aws/eks/terraform/opea-chatqna.tfvars
+++ cloud-service-provider/aws/eks/terraform/opea-chatqna.tfvars
@@ -2,3 +2,4 @@ cluster_name = "opea-chatqna"
 instance_types = ["I7ie.8xlarge"]
 capacity_type = "SPOT" # cheaper
 disk_size = 50
+region = "us-east-1"
--- cloud-service-provider/aws/eks/terraform/variables.tf
+++ cloud-service-provider/aws/eks/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
