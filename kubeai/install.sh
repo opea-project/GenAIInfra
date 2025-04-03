39181c644a843856a5ea51ff7b4fc4ec899f5e8d
Sakari Poussa <sakari.poussa@intel.com>
2025-04-03 14:42:53 +0300

    Introduce KubeAI for OPEA
    Signed-off-by: Sakari Poussa <sakari.poussa@intel.com>

---
+++ kubeai/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ -n "$HF_TOKEN" ]; then
+    echo "Using HF_TOKEN env var"
+elif [ -n "$1" ] && [ -f "$1" ]; then
+    echo "Using given HF token file"
+    HF_TOKEN=$(cat $1)
+else
+    echo "Usage: [HF_TOKEN=<token>] ${0##*/} [hf-token file]"
+    echo "ERROR: HF token missing!"
+    exit 1
+fi
+
+helm upgrade --install opea -n kubeai kubeai/kubeai \
+    --create-namespace \
+    --set secrets.huggingface.token=$HF_TOKEN \
+    -f $DIR/opea-values.yaml
