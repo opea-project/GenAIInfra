372417599efe75833053215296e4cde5806a71cc
Eze Lanza (Eze) <40643766+ezelanza@users.noreply.github.com>
2025-04-18 04:53:40 -0400

    Fix EKS connectivity issue in Terraform configuration (#999)
    * Update main.tf
    
    Typo fix
    
    Signed-off-by: Ezequiel Lanza <ezequiel.lanza@gmail.com>
    
    * Fix EKS connectivity issue in Terraform configuration
    
    Signed-off-by: Ezequiel Lanza <ezequiel.lanza@gmail.com>
    
    ---------
    
    Signed-off-by: Ezequiel Lanza <ezequiel.lanza@gmail.com>

--- cloud-service-provider/aws/eks/terraform/main.tf
+++ cloud-service-provider/aws/eks/terraform/main.tf
@@ -3,7 +3,19 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_availability_zones" "available" {
